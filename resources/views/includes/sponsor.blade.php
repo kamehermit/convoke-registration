be2598c6eac6992a6fb85e9899431d4ccd532153
animeshkuzur <animesh.kuzur@outlook.com>
2017-10-17 13:03:08 +0530

    sponsors link added

--- resources/views/includes/sponsor.blade.php
+++ resources/views/includes/sponsor.blade.php
@@ -2,7 +2,7 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12" align="center">
-					<img src="{{ URL::asset('/images/sponsors.png') }}" class="img-responsive">	
+					<a href="http://convoke.tech/#sponsors"><img src="{{ URL::asset('/images/sponsors.png') }}" class="img-responsive"></a>	
 				</div>
 			</div>
 		</div>
